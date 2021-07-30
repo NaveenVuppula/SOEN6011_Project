78a67d4628008ee64fac3db07adf0eae1014b898
NaveenVuppula <naveen90.v@gmail.com>
2021-07-31 00:30:13 +0530

    F5 unit test file.

---
+++ SOEN6011/test/F5Test.java
@@ -0,0 +1,95 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+public class F5Test {
+    
+    F5 f5obj = new F5();
+    
+    @Test
+    public void test1() {
+        double a = 2;
+        double base = 3;
+        int power = 6;
+        Assert.assertEquals(1458.0, f5obj.calc_F5(a, base, power), 0.00000000001);
+    }
+    
+    @Test
+    public void test2() {
+        double a = 6;
+        double base = 0;
+        int power = 0;
+        Assert.assertEquals(6.0, f5obj.calc_F5(a, base, power), 0.00000000001);
+    }
+    
+    @Test
+    public void test3() {
+        double a = 518;
+        double base = 5;
+        int power = 0;
+        Assert.assertEquals(518.0, f5obj.calc_F5(a, base, power), 0.00000000001);
+    }
+    
+    @Test
+    public void test4() {
+        double a = 125;
+        double base = 0;
+        int power = 5;
+        Assert.assertEquals(0.0, f5obj.calc_F5(a, base, power), 0.00000000001);
+    }
+    
+    @Test(expected = IllegalArgumentException.class)
+    public void test5() {
+        double a = 89;
+        double base = 0;
+        int power = -4;
+        f5obj.calc_F5(a, base, power);
+    }
+    
+    @Test
+    public void test6() {
+        double a = -10;
+        double base = 2;
+        int power = 6;
+        Assert.assertEquals(-640.0, f5obj.calc_F5(a, base, power), 0.00000000001);
+    }
+    
+    @Test
+    public void test7() {
+        double a = 10;
+        double base = 2;
+        int power = -6;
+        Assert.assertEquals(0.15625, f5obj.calc_F5(a, base, power), 0.00000000001);
+    }
+    
+    @Test
+    public void test8() {
+        double a = 10;
+        double base = -2;
+        int power = 6;
+        Assert.assertEquals(640.0, f5obj.calc_F5(a, base, power), 0.00000000001);
+    }
+    
+    @Test
+    public void test9() {
+        double a = -20;
+        double base = -2;
+        int power = -5;
+        Assert.assertEquals(0.625, f5obj.calc_F5(a, base, power), 0.00000000001);
+    }
+    
+    @Test
+    public void test10() {
+        double a = 134520;
+        double base = 5;
+        int power = 256;
+        Assert.assertEquals(1.1617373940313053E184, f5obj.calc_F5(a, base, power), 0.00000000001);
+    }
+    
+    @Test
+    public void test11() {
+        double a = 520;
+        double base = 6;
+        int power = -156;
+        Assert.assertEquals(2.110611e-119, f5obj.calc_F5(a, base, power), 0.00000000001);
+    }
+}
